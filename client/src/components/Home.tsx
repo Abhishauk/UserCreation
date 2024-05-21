28aa60bb426462ca0991d954fa0723bb1ecf9cb3
Abhishauk <ukabhisha@gmail.com>
2024-05-21 23:41:53 +0530

    latest

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import './style.css';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Welcome...</h1>
       <div className="images">
-        <Link to='/signup'>
-        <img src="/signup.png" alt="Sign Up" className="image" />
+        <Link to="/signup">
+          <img src="/signup.png" alt="Sign Up" className="image" />
         </Link>
-        <Link to='/signin'>
-        <img src="/signin.png" alt="Sign In" className="image" />
+        <Link to="/signin">
+          <img src="/signin.png" alt="Sign In" className="image" />
         </Link>
-        
       </div>
     </div>
   );
--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MainPage = () => {
   return (
     <div>
-        <h1>Welcome Page</h1>
+        <h1>Welcome to our Page</h1>
       
     </div>
   )
